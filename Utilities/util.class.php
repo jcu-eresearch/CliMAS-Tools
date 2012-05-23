c7cdd1614464b0f9bf0f3b1b3603555a8227ec87
jc166922 <jc166922@afakes-eresearch.jcu.edu.au>
2012-05-23 16:03:21 +1000

    Back to working state

--- Utilities/util.class.php
+++ Utilities/util.class.php
@@ -1062,6 +1062,23 @@ SQL;
         return $default;
     }
 
+
+    public static function startsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    public static function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+
+        $start  = $length * -1; //negative
+        return (substr($haystack, $start) === $needle);
+    }
     
 }
 ?>
