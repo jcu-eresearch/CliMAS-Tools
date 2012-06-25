67bc7e2f7338c2d0281e57b919a060d1ecef61fc
Daniel Baird <danielbaird@gmail.com>
2012-06-25 17:10:16 +1000

    fixed image paths

--- Search/DataDownload.php
+++ Search/DataDownload.php
@@ -459,16 +459,16 @@ function zipFiles($requestedData)
 
     <div class="credits">
         <a href="http://www.jcu.edu.au/ctbcc/">
-            <img src="/images/ctbcc_sm.png" alt="Centre for Tropical Biodiversity and Climate Change">
+            <img src="../images/ctbcc_sm.png" alt="Centre for Tropical Biodiversity and Climate Change">
         </a>
         <a href="http://www.tyndall.ac.uk/">
-            <img src="/images/themenews_logo.jpg" alt="Tyndall Centre for Climate Change Research">
+            <img src="../images/themenews_logo.jpg" alt="Tyndall Centre for Climate Change Research">
         </a>
         <a href="http://www.jcu.edu.au">
-            <img src="/images/jcu_logo_sm.png" alt="JCU Logo">
+            <img src="../images/jcu_logo_sm.png" alt="JCU Logo">
         </a>
         <a href="http://eresearch.jcu.edu.au/">
-            <img src="/images/eresearch.png" alt="eResearch Centre, JCU">
+            <img src="../images/eresearch.png" alt="eResearch Centre, JCU">
         </a>
     </div>
 
