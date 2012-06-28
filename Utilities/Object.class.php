ab5503a81ff87f4b3eb9626d2addd2f15cbc7190
jc166922 <jc166922@afakes-eresearch.jcu.edu.au>
2012-06-28 15:28:00 +1000

    Added tdh-tools-2 config for both Webserver and HPC sides
    HPC location:  /scratch/jc166922/tdh2
    webserver (tdh-tools-2) :  /www/html/eresearch/TDH-Tools

--- Utilities/Object.class.php
+++ Utilities/Object.class.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Allows for Support of "Properties"
+ * Public properties are defined as Methods()
+ *  - if no peremeters are passed to the method then it returns the current value
+ *  - if a parameters is passed than this is set to the value of the propery
+ * 
+ * - All obejcts that are create have a uniqueID
+ * 
+ *  public function ReadWriteProperty() {
+ *        if (func_num_args() == 0) return $this->getProperty();
+ *        return $this->setProperty(func_get_arg(0));
+ *  }
+ *
+ * 
+ */
 class Object  {
 
 
@@ -6,6 +21,13 @@ class Object  {
     
     private $property = array();
     
+    
+    
+    /**
+     * Create an object 
+     * 
+     * @param type $id  - if we already have a tracking ID we can pass it in
+     */
     public function __construct($id = null) {    
         
         
@@ -26,11 +48,24 @@ class Object  {
         $this->property = null;
     }
 
+    /**
+     * When extend from a subclass will return the SubClass Clas Name
+     * 
+     * @return string the class type 
+     */
     public function _ClassName() {
         return get_class($this);
     }
 
 
+    /**
+     * set value of property
+     * Called from Subclass
+     * - property name is found by a debug backtrace to see what function / method name called this function
+     * 
+     * @param mixed $value Property value
+     * @return mixed Property value 
+     */
     protected function setProperty($value) {
         $bt = debug_backtrace(); 
         $property_name = $bt[1]['function'];
@@ -39,12 +74,25 @@ class Object  {
         return $this->property[$property_name];  //** return current vlaue of property
     }
 
+    /*
+     *  Setting a property value from somewhere else 
+     *  within the class you should use  $this->PropertyName($value)
+     *  - This method alows updating of multiple properties via loops
+     *  - Also allows for dynamic creation of properties
+     * 
+     * 
+     */
     protected function setPropertyByName($property_name,$value) {
         if (!array_key_exists($property_name, $this->property)) $this->property[$property_name] = null; //** make the property exist if it does not
         $this->property[$property_name] = $value; //** set property value //** if they passed value the set that value
         return $this->property[$property_name];  //** return current vlaue of property
     }
 
+    /**
+     * Get value  of property
+     * 
+     * @return mixed current value of propery
+     */
     protected function getProperty() {
         $bt = debug_backtrace(); 
         $property_name = $bt[1]['function'];
@@ -52,6 +100,12 @@ class Object  {
         return $this->property[$property_name];  //** return current vlaue of property
     }
 
+    /*
+     *  getting a property value from somewhere else 
+     *  within the class you should use  $this->PropertyName()
+     *  - This method allows reading of multiple properties via loops
+     * 
+     */    
     public function getPropertyByName($property_name,$null_value = null)
     {
         if (!array_key_exists($property_name, $this->property)) return $null_value;
@@ -59,12 +113,19 @@ class Object  {
     }
 
 
+    /** 
+     * Check object to see if property exists
+     * 
+     * @param string $property_name Name of property to check for
+     * @return boolean Has this property 
+     */
     protected function hasProperty($property_name) {
         
         if (!array_key_exists($property_name, $this->property))  return true;
         
         return false;
     }
+    
     
     
     protected function readOnly() {
@@ -74,11 +135,20 @@ class Object  {
         return $this->property[$property_name];  //** return current vlaue of property
     }
     
+    /**
+     * Unique ID of object or ID passed in at creation
+     * 
+     * @return mixed Object ID
+     */
     public function ID() {
         return $this->getProperty();
     }
     
     
+    /**
+     * String version of objects properties
+     * @return string  
+     */
     public function __toString() {
         
         $result = $this->name."::\n";
@@ -88,6 +158,10 @@ class Object  {
         return $result;
     }
 
+    /**
+     * 
+     * @return array string arry of property names 
+     */
     public function PropertyNames()
     {
         $result = array_flip(array_keys($this->property));
@@ -95,6 +169,11 @@ class Object  {
         return array_keys($result);
     }
 
+    
+    /**
+     * 
+     * @return array string arry of property values 
+     */
     public function PropertyValues()
     {
         $result = array();
@@ -107,6 +186,11 @@ class Object  {
 
 
 
+    /**
+     * When debugging used to output current properies and values
+     * 
+     * @param type $web 
+     */
     public function Debug($web = false)
     {
         $eol = ($web) ? "<br>\n" : "\n";
@@ -169,65 +253,31 @@ class Object  {
     }
     
     
+    
     public function DataName()
     {
         return $this->name;
     }
     
+    /**
+     * Create copy of this object  into $dest
+     * - create a now object and copy property values
+     * 
+     * @param Object $dest 
+     */
     protected function copy(Object &$dest) 
     {                
         foreach ($this->property as $key => $value) 
             $dest->property[$key] = $value;        
     }
     
-    
-
-//    /**
-//     * Overloads
-//     * ()            = return Tags array
-//     * ($key)        = return Value of this Tag
-//     * ($key,$value) = Add new Tag ($key) and set it's value ot ($value)
-//     * ($array)      = Foreach the array and add tags as per  $array's key/**values
-//     *
-//     */
-//    public function Tags()
-//    {
-//
-//        if (func_num_args() == 0) return $this->getProperty(); //** return all tags
-//
-//        if (func_num_args() == 1 && !is_array(func_get_arg(0)))
-//        {
-//            //** single
-//            $tag_array = $this->getProperty();
-//            if (!array_key_exists(func_get_arg(0), $tag_array)) return null;  //** if the key does not exist then return null
-//            return $tag_array[func_get_arg(0)];
-//        }
-//
-//        //** they sent an array as the first parameter - so assum they want to add this array as tags
-//        if (func_num_args() == 1 && is_array(func_get_arg(0)))
-//        {
-//            $tag_array = $this->getProperty();
-//            foreach (func_get_arg(0) as $key => $value)
-//            {
-//                $tag_array[$key] = $value;
-//                $this->setProperty($tag_array);
-//            }
-//        }
-//
-//
-//        //** two parameters - so add new tag with Key and Value
-//        if (func_num_args() >= 2)
-//        {
-//            $tag_array = $this->getProperty();
-//            $tag_array[func_get_arg(0)] = func_get_arg(1);
-//            $this->setProperty($tag_array);
-//        }
-//
-//
-//    }
 
     
-
+    /**
+     * Inititalise the values of the properties of this object from a keyed array
+     * @param array $src array(key1 => Value1, key2 => Value2, .....  )
+     * @return null 
+     */
     public function InitaliseByArray($src)
     {
         if (!is_array($src)) return null;
@@ -237,18 +287,29 @@ class Object  {
     }
  
     
+    /**
+     * Text to see if $src is of Class object or a subclass of Object
+     * 
+     * @param mixed $src
+     * @return bool
+     */
     public static function isObject($src)
     {
         return ($src instanceof self);
     }
 
 
+    /**
+     * Cast $src to b an Object
+     * 
+     * @param Object $src
+     * @return Object 
+     */
     public static function cast($src)
     {
         $src instanceof Object;
         return $src;
     }
-
 
 
     public static $TRUE = "TRUE";
--- doc/docs.sh
+++ doc/docs.sh
@@ -1,5 +1,11 @@
 
-phpdoc -d /www/eresearch/TDH-Tools/doc/Search/ -t /www/eresearch/TDH-Tools/doc/doc/Search/
-phpdoc -d /www/eresearch/TDH-Tools/doc/Utilities/ -t /www/eresearch/TDH-Tools/doc/doc/Utilities/
+#phpdoc run -d /www/eresearch/TDH-Tools/Search -t /www/eresearch/TDH-Tools/doc/Search --defaultpackagename AP02
+#phpdoc run -d /www/eresearch/TDH-Tools/Utilities -t /www/eresearch/TDH-Tools/doc/Utilities --defaultpackagename AP02
 
-git push
+if [ "$1" = "git" ]
+then
+    echo "Commiting updated documents to git-hub"
+    git commit -m "documentation updated using phpDocumentor"
+    git push
+fi
+
