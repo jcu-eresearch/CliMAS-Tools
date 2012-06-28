4aa80eda66c06a38ab08f7251a1ac41872d9c023
Adam Fakes <jc166922@afakes-eresearch.jcu.edu.au>
2012-06-28 10:43:25 +1000

    Add phpDocumetor creation script

---
+++ doc/docs.sh
@@ -0,0 +1,5 @@
+
+phpdoc -d /www/eresearch/TDH-Tools/doc/Search/ -t /www/eresearch/TDH-Tools/doc/doc/Search/
+phpdoc -d /www/eresearch/TDH-Tools/doc/Utilities/ -t /www/eresearch/TDH-Tools/doc/doc/Utilities/
+
+git push
