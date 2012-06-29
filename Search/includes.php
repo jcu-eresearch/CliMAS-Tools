157a3aac77e38611fe8dcd8300b3a5bce078c6ef
jc166922 <jc166922@afakes-eresearch.jcu.edu.au>
2012-06-29 16:18:02 +1000

    config fixes

--- Search/includes.php
+++ Search/includes.php
@@ -1,6 +1,5 @@
 <?php
-include_once 'ParameterNames.class.php';
-include_once 'configuration.class.php';
+include_once dirname(__FILE__).'/ParameterNames.class.php';
 
 $conf = array();
 
@@ -15,21 +14,22 @@ if (file_exists("config.tdh2"))     include_once 'config.tdh2';
 if (file_exists("config.tdh2-hpc")) include_once 'config.tdh2-hpc';
 if (file_exists("config.afakes"))   include_once 'config.afakes';
 
-    
-$af = configuration::ApplicationFolder();
+include_once dirname(__FILE__).'/configuration.class.php';
+
+$af = dirname(__FILE__).'/';
 
 include_once configuration::UtilityClasses();
 
-include_once $af.'Search/Session.class.php';
-include_once $af.'Search/CommandAction/Command.includes.php';
-include_once $af.'Search/Finder/Finder.includes.php';
-include_once $af.'Search/Data/Data.includes.php';
-include_once $af.'Search/extras/extras.includes.php';
-include_once $af.'Search/MapServer/Mapserver.includes.php';
-include_once $af.'Search/DB/ToolsData.includes.php';
-include_once $af.'Search/Output/Output.includes.php';
+include_once $af.'Session.class.php';
+include_once $af.'CommandAction/Command.includes.php';
+include_once $af.'Finder/Finder.includes.php';
+include_once $af.'Data/Data.includes.php';
+include_once $af.'extras/extras.includes.php';
+include_once $af.'MapServer/Mapserver.includes.php';
+include_once $af.'DB/ToolsData.includes.php';
+include_once $af.'Output/Output.includes.php';
 
-include_once $af.'Search/Finder/Species/SpeciesMaxent.action.class.php';
+include_once $af.'Finder/Species/SpeciesMaxent.action.class.php';
 
 
 
