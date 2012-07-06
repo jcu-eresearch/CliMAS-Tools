ab374eea6d28005db436f190fb756fc8cdc86f44
Adam Fakes <Adam.Fakes@afakes.jcu.edu.au>
2012-07-06 16:45:58 +1000

    Removed Password

--- Search/DB/ToolsData.configuration.class.php
+++ Search/DB/ToolsData.configuration.class.php
@@ -7,15 +7,10 @@
  */
 class ToolsDataConfiguration {
 
-    public static function Server()  { return "localhost"; }
-    public static function Username() { return "jc166922"; }
-    public static function Password() { return "Volts100."; }
-    public static function Database() {return "TDH-TOOLS";}
 
     public static function ALAFullTextSearch() { return  "http://bie.ala.org.au/search?q=";}
 
     public static function ALAFullTextSearchJSON() { return  "http://bie.ala.org.au/search.json?q=";}
-
     
     public static function Species_DB_Server()   { return "tdh-tools-2.hpc.jcu.edu.au"; }
     public static function Species_DB_Port()     { return "5432"; }
