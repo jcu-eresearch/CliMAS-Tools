b39bd8ddfc0200cb4d110878105699f83b4fb756
Adam Fakes <Adam.Fakes@afakes.jcu.edu.au>
2012-10-26 11:17:39 +1000

    Created Config to run HPC side php on HPC

--- applications/configure.php
+++ applications/configure.php
@@ -84,64 +84,18 @@ if (!is_dir(configuration::Maxent_Taining_Data_folder()  ))
     echo "WARNING:: Maxent_Taining_Data_folder does not exist: ".configuration::Maxent_Taining_Data_folder()."\n";    
 }
 
-if (!is_dir(configuration::Maxent_Future_Projection_Data_folder()  ))
-{
-    echo "WARNING:: Maxent_Future_Projection_Data_folder does not exist: ".configuration::Maxent_Future_Projection_Data_folder()."\n";
-}
+// OBSOLETE 2012-10-26 Adam Fakes - I have set the appropriate folder names bur these are obselete now
+//if (!is_dir(configuration::Maxent_Future_Projection_Data_folder()  ))
+//{
+//    echo "WARNING:: Maxent_Future_Projection_Data_folder does not exist: ".configuration::Maxent_Future_Projection_Data_folder()."\n";
+//}
 
 if (!is_dir(configuration::Maxent_Species_Data_folder()  ))
 {
     echo "WARNING:: Maxent_Species_Data_folder() does not exist: ".configuration::Maxent_Species_Data_folder()."\n";
 }
 
-$incoming_sh = configuration::ApplicationFolder()."applications/Incoming.sh";
-    
-echo "=====================================================================================\n";  
-echo "Check on database access here\n";
 
-
-
-$CRON_REQUIRED = false;
-
-if ($CRON_REQUIRED)
-{
-    
-    // If required the receciveing server will wait for incoming cxommands 
-    // CRON job will read queue and execute any commands with  EXECUTION FLAG =  READY 
-    
-    echo "\n";
-    echo "Create CRON script {$incoming_sh} \n";
-    echo "\n";
-
-    file_put_contents($incoming_sh, "#!/bin/tcsh\n cd ".configuration::ApplicationFolder()."\n php -q ".configuration::ApplicationFolder()."applications/Incoming.php\n\n");
-
-    if (!file_exists($incoming_sh))
-    {
-        echo "FAILED: to create CRON script {$incoming_sh} \n";
-        exit(1);
-    }
-
-    echo "\n make executable  chmod u+x {$incoming_sh}";
-    exec("chmod u+x {$incoming_sh}");
-
-    echo "\n";
-
-    echo "\nCRON (on host that will be processing the queue)";
-    echo "\n";
-    echo "\n  copy |* * * * * {$incoming_sh}|";
-    echo "\n  crontab -e\n";
-    echo "\n  i = insert\n";
-    echo "\n  paste";
-    echo "\n  ESC";
-    echo "\n  :w<enter>";
-    echo "\n  ESC";
-    echo "\n  :q<enter>";
-    echo "\n";
-    echo "=====================================================================================\n";
-    echo "Seems to be OK, check warnings if any\n";
-    echo "\n";
-
-}
 
 
 ?>
