2b2b3c2f1ea4ca72f6b62282d0b28a5286d54200
Adam Fakes <Adam.Fakes@afakes.jcu.edu.au>
2012-10-26 16:38:31 +1000

    Updated config for new Apache config

--- applications/includes.php
+++ applications/includes.php
@@ -24,13 +24,10 @@ $hostname = trim(exec("hostname --fqdn"));
 if (file_exists("{$af}config.default"))  include_once $af.'config.default';
 
 if (file_exists("{$af}config.daniel"))   include_once $af.'config.daniel';
-//if (file_exists("{$af}config.hpc"))      include_once $af.'config.hpc';
-//if (file_exists("{$af}config.wallace"))  include_once $af.'config.wallace';
 if (file_exists("{$af}config.tdh1"))     include_once $af.'config.tdh1';
 if (file_exists("{$af}config.tdh1-hpc")) include_once $af.'config.tdh1-hpc';
 if (file_exists("{$af}config.tdh2"))     include_once $af.'config.tdh2';
 if (file_exists("{$af}config.tdh2-hpc")) include_once $af.'config.tdh2-hpc';
-if (file_exists("{$af}config.home.tdh")) include_once $af.'config.home.tdh';
 
 include_once dirname(__FILE__).'/configuration.class.php';
 
