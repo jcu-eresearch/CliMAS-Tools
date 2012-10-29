499801e818c1808b3cffbfa145e6faaed7bef6ba
Adam Fakes <Adam.Fakes@afakes.jcu.edu.au>
2012-10-29 10:03:14 +1000

    Safer version of create quiuck looks

--- applications/ALApart3.php
+++ applications/ALApart3.php
@@ -4,6 +4,27 @@ $inc_folder = dirname(__FILE__);
 include_once "{$inc_folder}/includes.php";
 if (php_sapi_name() != "cli") return;
 
+
+$execute = false;
+
+$execute_flag = array_util::Value($argv, 1);
+if (is_null($execute_flag)) $execute_flag = 'NO_EXECUTE';
+
+
+if ($execute_flag !== 'EXECUTE')
+{
+    ErrorMessage::Marker("####### DRY RUN ONLY .... no files will be changed #######");
+    ErrorMessage::Marker("Please run as  'php {$argv[0]} EXECUTE'  to actually execute and do something ");    
+    $execute = false;
+}
+else
+{
+    ErrorMessage::Marker("####### EXECUTING DATA BUILD #######");
+    $execute = true;
+}
+
+
+
 $JSON_KEY = 'JSON';
 $clazz_translation = array();
 $clazz_translation['AMPHIBIA'] = 'amphibians';
@@ -25,8 +46,8 @@ foreach ($folders as $folder_basename => $folder_pathname)
 {
     ErrorMessage::Marker("RICHNESS QuickLook [{$folder_basename}] {$count}/".count($folders));
     $cmd = "php {$inc_folder}/HotSpotsCreateQuickLook.php --cmd='find {$folder_pathname}/*.asc.gz | sort ' --output={$folder_pathname} --title='Predicted Species Richness for the genus {$folder_basename}'";
-    // ErrorMessage::Marker("{$cmd}");
-    exec($cmd);    
+    ErrorMessage::Marker("{$cmd}");
+    if ($execute) exec($cmd);    
     
     $count++;
     
