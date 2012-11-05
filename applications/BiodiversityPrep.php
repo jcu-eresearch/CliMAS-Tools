5aaf0017aaa87fab1af1d120f8f75d8e0e2ca8d1
Daniel Baird <danielbaird@gmail.com>
2012-11-05 16:10:59 +1000

    fixing layer naming

--- applications/BiodiversityPrep.php
+++ applications/BiodiversityPrep.php
@@ -92,8 +92,7 @@ if (file_exists($map_path)) {
 }
 
 $layer = $M->Layers()->AddLayer($grid_filename_asc);
-$layer->set('name', 'maplayer');
-
+$layer->name = "maplayer";
 $layer instanceof MapServerLayerRaster;
 $layer->HistogramBuckets($bucket_count);
 
