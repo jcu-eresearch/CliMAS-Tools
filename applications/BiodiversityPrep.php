cf4f2cf107be2212cc24a35c46974e29437135f4
Daniel Baird <danielbaird@gmail.com>
2012-11-05 15:59:17 +1000

    fixing layer naming

--- applications/BiodiversityPrep.php
+++ applications/BiodiversityPrep.php
@@ -92,10 +92,11 @@ if (file_exists($map_path)) {
 }
 
 $layer = $M->Layers()->AddLayer($grid_filename_asc);
+$layer->set('name', 'maplayer');
+
 $layer instanceof MapServerLayerRaster;
 $layer->HistogramBuckets($bucket_count);
 
-$layer->set('name', 'maplayer');
 
 // start ramp at Zero -
 $ramp = RGB::Ramp(0, 1, $bucket_count,RGB::ReverseGradient(RGB::GradientYellowOrangeRed()));
