dbab923d83079bbe70f33f11293ca26d2abed5b5
Daniel Baird <danielbaird@gmail.com>
2012-11-26 13:57:33 +1000

    maybe this fixes the inversion of scale

--- applications/MapServer/Mapfile.class.php
+++ applications/MapServer/Mapfile.class.php
@@ -169,11 +169,7 @@ class Mapfile extends Object{
                 $expression = "EXPRESSION ([pixel] = {$value1})";
             } else {
                 $expression_name = "NAME \"{$value1} .. {$value2}\"";
-                if ($index == count($values)) {
-                    $expression = "EXPRESSION ([pixel] >= {$value1} and [pixel] =< {$value2})";
-                } else {
-                    $expression = "EXPRESSION ([pixel] >= {$value1} and [pixel] < {$value2})";
-                }
+                $expression = "EXPRESSION ([pixel] > {$value1} and [pixel] =< {$value2})";
             }
 
 $c = <<<CLASS
