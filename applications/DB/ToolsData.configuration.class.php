636ffbb0a605796ed9906569185d47624b805998
Daniel Baird <danielbaird@gmail.com>
2012-12-19 12:45:29 +1000

    database settings

--- applications/DB/ToolsData.configuration.class.php
+++ applications/DB/ToolsData.configuration.class.php
@@ -2,16 +2,16 @@
 /**
  * database configuration and default values
  *
- * TODO:: Needs to come from Config file that is specific to HOstname
+ * TODO:: Needs to come from Config file that is specific to Hostname
  *
  */
 class ToolsDataConfiguration {
 
-    public static function Species_DB_Server()   { return "tdh-tools-2.hpc.jcu.edu.au"; }
+    public static function Species_DB_Server()   { return "localhost"; }
     public static function Species_DB_Port()     { return "5432"; }
-    public static function Species_DB_Username() { return "ap02"; }
+    public static function Species_DB_Username() { return "climas"; }
     public static function Species_DB_Password() { return "71a6e5db6b9cfda9f0af062254b5bfbe"; }
-    public static function Species_DB_Database() { return "ap02_new"; }
+    public static function Species_DB_Database() { return "climas_production"; }
 
 }
 
