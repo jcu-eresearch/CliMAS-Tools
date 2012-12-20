80e3e2d1054317c2fe2cdef7366aa09f94a2c47c
Daniel Baird <danielbaird@gmail.com>
2012-12-20 13:59:05 +1000

    adding google analytics tracking code

--- applications/ToolsFooter.php
+++ applications/ToolsFooter.php
@@ -5,3 +5,18 @@
     This work is licensed under a
     <a rel="license" href="http://creativecommons.org/licenses/by/3.0/au/deed.en_GB">Creative Commons Attribution 3.0 Australia License</a>.
 </div></div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16452055-13']);
+  _gaq.push(['_setDomainName', 'tropicaldatahub.org']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
