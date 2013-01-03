d53c73458da53d73a7de90e1eb2362780e9eeff1
Daniel Baird <danielbaird@gmail.com>
2013-01-03 11:26:26 +1000

    making biodiversity lists

--- applications/setupData.php
+++ applications/setupData.php
@@ -270,14 +270,16 @@ foreach ($species_list as $species_name => $species_data) {
     $species_list_dir = $data_root . 'species/';
     $species_web_dir = $http_data_root . 'species/';
     $all_list = array();
+    $all_list[] = "Species Name\tSpecies Data URL";
     foreach(glob($species_list_dir . '*_*') as $spdir) {
         if (is_dir($spdir)) {
             ErrorMessage::Progress();
             $spname = basename($spdir);
-            $all_list[] = "{$species_web_dir}{$spname}/species_data_{$spname}.zip";
+            $nicename = str_replace('_', ' ', $spname);
+            $all_list[] = "{$nicename}\t{$species_web_dir}{$spname}/species_data_{$spname}.zip";
         }
     }
-    write_file($data_root . 'all_vertebrates.txt', implode("\n", $all_list));
+    write_file($data_root . 'all_vertebrates.tabseparated.csv', implode("\n", $all_list));
 }
 
 ErrorMessage::EndProgress();
