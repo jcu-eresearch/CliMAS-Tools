738b90d0fcffb017bd96ae0a23e820a978c679b3
Daniel Baird <danielbaird@gmail.com>
2013-02-26 14:30:04 +1000

    Removed unused code

--- applications/SpeciesSuitabilityDownload.php
+++ applications/SpeciesSuitabilityDownload.php
@@ -13,21 +13,6 @@ $filename = $zipfiles[0];
 
 if (is_file($filename)) {
 
-    /*
-    http_send_content_disposition(basename($filename), true);
-    http_send_content_type();
-    // http_throttle(0.1, 2048);
-    http_send_file($filename);
-    */
-
-    /*
-    header("X-Sendfile: {$filename}");
-    header("Content-type: application/octet-stream");
-    header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
-    header("Content-Length: ". filesize($filename));
-    readfile($filename);
-    */
-
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename='.basename($filename));
